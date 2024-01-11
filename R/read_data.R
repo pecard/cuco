31cf49ced3c82b498e6ada846fe9c7419a8d295e
Paulo Cardoso <pcardoso@bio3.office>
2024-01-11 00:27:49 +0000

    plot adjustment

--- R/read_data.R
+++ R/read_data.R
@@ -7,14 +7,25 @@ file1 <-
   read.csv(here::here(gdrv, 'aves_cucoR.csv'), sep = ';') %>%
   janitor::clean_names() %>%
   pivot_longer(cols = starts_with("x"), names_to = 'data', values_to = 'num')
+
 file1$data <- dmy(sub('x', '', file1$data))
 
 unique(file1$nome_comum)
 
 file1 %>%
-  filter(nome_comum %in% c("Abelharuco","Abibe", "Lugre", "Tentilhão")) %>%
+  filter(nome_comum %in% c("Cuco","Pardal-espanhol" , 
+                           "Pombo-torcaz","Abelharuco",
+                           "Abibe", "Lugre", 
+                           "Tentilhão")) %>%
   ggplot() +
   geom_col(aes(data, num), width = 10) +
   facet_grid(nome_comum~., scales = "free") +
   scale_x_date(breaks = '3 months') +
+  theme_bw() +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        #panel.border = element_blank(),
+        panel.background = element_blank()) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
